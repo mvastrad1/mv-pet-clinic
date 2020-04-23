c8b0838a4d96eb37ee594db47b435e13c3e0c2d2
mvastrad1 <mvastrad@yahoo.com>
2020-04-23 16:45:01 -0500

    Initial project created

---
+++ src/main/java/mv/spring/mvpetclinic/MvPetClinicApplication.java
@@ -0,0 +1,13 @@
+package mv.spring.mvpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvPetClinicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mv/spring/mvpetclinic/MvPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package mv.spring.mvpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
